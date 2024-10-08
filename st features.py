11a5499913e8af14d3ef630e791f3cac2c9f7d90
JiaGuo31 <guo.hammersmith@gmail.com>
2024-10-09 00:14:58 +0800

    First commit

---
+++ st features.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+# Use the get method since the keys won't be in session_state 
+# on the first script run 
+if st.session_state.get('clear'): 
+	st.session_state['name'] = '' 
+	
+if st.session_state.get('streamlit'): 
+	st.session_state['name'] = 'Streamlit' 
+	
+st.text_input('Name', key='name') 
+
+# assign a key to a button, you can condition code on a button's state by using its value in st.session_state
+st.button('Clear name', key='clear') 
+st.button('Streamlit!', key='streamlit')
